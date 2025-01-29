97e933329e8afb861c3675a1df0c900b2d8d7dc2
ozaki98 <k.ozaki@insel-art.jp>
2025-01-29 14:31:03 +0900

    変数名変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,15 @@ import netlify from '@astrojs/netlify';
 export default defineConfig({
   vite: {
     define: {
-      'import.meta.env.MICROCMS_SERVICE_DOMAIN': 
-        JSON.stringify(process.env.MICROCMS_SERVICE_DOMAIN),
-      'import.meta.env.MICROCMS_API_KEY': 
-        JSON.stringify(process.env.MICROCMS_API_KEY),
+      "import.meta.env.VITE_MICROCMS_SERVICE_DOMAIN": JSON.stringify(
+        process.env.VITE_MICROCMS_SERVICE_DOMAIN
+      ),
+      "import.meta.env.VITE_MICROCMS_API_KEY": JSON.stringify(
+        process.env.VITE_MICROCMS_API_KEY
+      ),
     },
   },
   integrations: [react()],
-  output: 'server',
+  output: "server",
   adapter: netlify(),
 });
