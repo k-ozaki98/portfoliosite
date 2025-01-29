b766e1a73acd61683ae4055906cb9fea1377dbe0
ozaki98 <k.ozaki@insel-art.jp>
2025-01-29 15:01:26 +0900

    変数変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,15 +6,17 @@ import cloudflare from "@astrojs/cloudflare";
 export default defineConfig({
   vite: {
     define: {
-      "import.meta.env.VITE_MICROCMS_SERVICE_DOMAIN": JSON.stringify(
-        process.env.VITE_MICROCMS_SERVICE_DOMAIN
+      "import.meta.env.MICROCMS_SERVICE_DOMAIN": JSON.stringify(
+        process.env.MICROCMS_SERVICE_DOMAIN
       ),
-      "import.meta.env.VITE_MICROCMS_API_KEY": JSON.stringify(
-        process.env.VITE_MICROCMS_API_KEY
+      "import.meta.env.MICROCMS_API_KEY": JSON.stringify(
+        process.env.MICROCMS_API_KEY
       ),
     },
   },
   integrations: [react()],
   output: "server",
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    mode: "directory", // これを追加
+  }),
 });
--- src/lib/microcms.ts
+++ src/lib/microcms.ts
@@ -2,8 +2,8 @@ import { createClient } from 'microcms-js-sdk';
 import type { MicroCMSQueries } from 'microcms-js-sdk';
 
 export const client = createClient({
-  serviceDomain: import.meta.env.VITE_MICROCMS_SERVICE_DOMAIN,
-  apiKey: import.meta.env.VITE_MICROCMS_API_KEY
+  serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: import.meta.env.MICROCMS_API_KEY
 });
 
 export type Category = {
